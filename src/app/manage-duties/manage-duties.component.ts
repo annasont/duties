65c1310fc20542a228a416b4ea51654d8c33f17f
Anna Sontowska <aniasontowska@gmail.com>
2021-11-09 18:21:26 +0100

    Created interface Duty, created fake temporary duties-data

--- src/app/manage-duties/manage-duties.component.ts
+++ src/app/manage-duties/manage-duties.component.ts
@@ -5,11 +5,53 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './manage-duties.component.html',
   styleUrls: ['./manage-duties.component.scss']
 })
+
 export class ManageDutiesComponent implements OnInit {
+
+  duties:Duty[] = [
+    {
+      title: 'Mycie okien',
+      frequency: Frequency.repeat,
+      frequencyNumber: 3,
+      frequencyUnit: FrequencyUnit['week(s)'],
+      dateStart: new Date,
+    },
+    {
+      title: 'Kupienie lodowki',
+      frequency: Frequency['one-time'],
+      dateStart: new Date,
+    },
+    {
+      title: 'Bazgranie',
+      frequency: Frequency.repeat,
+      frequencyNumber: 4,
+      frequencyUnit: FrequencyUnit['year(s)'],
+      dateStart: new Date,
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+interface Duty {
+  title: string
+  frequency: Frequency,
+  frequencyNumber?: number,
+  frequencyUnit?: FrequencyUnit,
+  dateStart: Date,
+}
+
+enum Frequency {
+  'one-time',
+  'repeat'
+}
+
+enum FrequencyUnit {
+  'week(s)',
+  'month(s)',
+  'year(s)',
 }
