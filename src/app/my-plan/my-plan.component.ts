c5608f36f236a43617e687e170399fa9841ff9d8
annasont <aniasontowska@gmail.com>
2021-11-21 09:26:44 +0100

    Starts working on my-plan

--- src/app/my-plan/my-plan.component.ts
+++ src/app/my-plan/my-plan.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DutiesService } from '../shared/services/duties.service';
+import { Duty } from '../shared/interfaces';
 
 @Component({
   selector: 'app-my-plan',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-plan.component.scss']
 })
 export class MyPlanComponent implements OnInit {
+  duties:Duty[] = []
 
-  constructor() { }
+
+  constructor(private dutiesService: DutiesService) { }
 
   ngOnInit(): void {
+    this.loadDuties();
+  }
+
+  loadDuties() {
+    this.dutiesService.all().subscribe(
+      (duties) => this.duties = duties.sort(function(a: Duty, b: Duty){
+      let textA: string = a.title.toUpperCase();
+      let textB: string = b.title.toUpperCase();
+      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    }),
+      (error) => console.log(`loadDuties error`, error)
+    );
   }
 
 }
